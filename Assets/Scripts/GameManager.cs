a3e7085cf4293d7cb48a8d6542066844d664a404
grez <96dev@protonmail.com>
2019-06-01 14:32:58 -0400

    side walk curb generating now

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static float worldSpeed = 20f;
+
     public static float xCreationMin = -400f;
     public static float xCreationMax = 400f;
     public static float ySpawn = 138.1f;
     public static float zSpawn = -58.56f;
     public static float xRotationSpawn = 157.021f;
-    public static float worldSpeed = 20f;
+
+    public static float roadSize = 80f;
 }
